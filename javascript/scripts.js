444f8dd510ec06e6bf39c899bd94a40a181875a5
elpaje00 <telesmoisesmendes@gmail.com>
2024-03-13 00:40:34 -0300

    Primeira tela em andamento

---
+++ javascript/scripts.js
@@ -0,0 +1,23 @@
+window.addEventListener("scroll", function() { //Cria um evento que chama uma função toda vez que a tela é rolada 
+    var h4Element = document.querySelector(".scroll"); //Uma variavel h4Element recebe uma procura no HTML por um elemento de classe scroll
+    var scrollPosition = window.scrollY; //Uma variavel scrollposition recebe a posição atual da tela em relação ao eixo Y
+    var newPos = Math.min(scrollPosition, window.innerWidth - 200); // Uma variavel newPos recebe a função Math.min(a, b), que retorna o menor valor entre a e b, menos 200 pixels, para que o h4 não passe mais de 200px para a direita
+
+    h4Element.style.left = newPos + "px"; //Define a nova posição do elemento de classe scroll como newPos e especifica como pixels  
+});
+
+window.addEventListener('scroll', function() { //Cria um evento que chama uma função toda vez que a tela é rolada
+    var sections = document.querySelectorAll('section'); //A variavel sections recebe todos os elementos sections do arquivo HTML
+    var indicator = document.getElementById('indicator'); //A variavel indicator recebe o elemento indicator do arquivo HTML
+    var scrollPosition = window.scrollY; // A variavel scrollPosition recebe a posição atual em relação ao eixo Y
+
+    sections.forEach(function(section) { //Loop que pega cada uma das sessões da variavel sections e executa algo com base nelas
+        var top = section.offsetTop; //A variavel top recebe a posição vertical (em pixels) do topo da seção em relação ao topo do documento
+        var bottom = top + section.offsetHeight; //
+        if (scrollPosition >= top && scrollPosition < bottom) {
+            var width = (section.offsetHeight / document.body.scrollHeight) * 100;
+            indicator.style.width = width + '%';
+            return;
+        }
+    });
+});
